291398652aee79a1f801be625eec5bca80eedddb
satinder <satinderkaura348@gmail.com>
2025-11-28 15:05:50 +1100

    vm size issue

--- infra/aks/main.tf
+++ infra/aks/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "standard_a2_v2"
   }
 
   identity {
